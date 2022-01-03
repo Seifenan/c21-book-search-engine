715d91c6741c76bcb59607b127c6110dc5cbcf5f
S317 <seifenan@gmail.com>
2022-01-02 22:32:52 -0500

    added mutations & queries to client

---
+++ client/src/utils/queries.js
@@ -0,0 +1,24 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        authors
+        description
+        title
+        bookId
+        image
+        link
+      }
+    }
+  }
+`;
+
+
+
+
