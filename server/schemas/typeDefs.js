8521182eb2098be93840415fa232dc94c1ee77c8
S317 <seifenan@gmail.com>
2022-01-02 21:46:06 -0500

    modified auth.js with GraphQL & server.js for Apollo; reorganized typedef to match criteria

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,37 +1,6 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type User {
-    _id: ID
-    username: String
-    email: String
-    savedBooks: [Book]
-    bookCount: Int
-  }
-
-  type Book {
-    bookId: String
-    authors: [String]
-    description: String
-    image: String
-    link: String
-    title: String
-  }
-
-  input savedBook {
-    bookId: String
-    authors: [String]
-    description: String
-    image: String
-    link: String
-    title: String
-  }
-
-  type Auth {
-    token: ID!
-    user: User
-  }
-
   type Query {
     me: User
   }
@@ -41,6 +10,37 @@ const typeDefs = gql`
     addUser(username: String!, email: String!, password: String!): Auth
     saveBook(input: savedBook!): User
     removeBook(bookId: ID!): User
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  input savedBook {
+    authors: [String]
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
   }
 `;
 
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+
 const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
@@ -9,20 +10,38 @@ const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const startServer = async () => {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware,
-  });
-  await server.start();
-  server.applyMiddleware({ app });
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-};
 
-startServer()
+// >>>>>>>>>>>>>
+// if using Apollo 3
 
-app.use(express.urlencoded({ extended: false }));
+// const startServer = async () => {
+//   const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//     context: authMiddleware,
+//   });
+//   await server.start();
+//   server.applyMiddleware({ app });
+// };
+
+// startServer()
+
+// <<<<<<<<<<<<<<>>>>>>>>>>>>>>
+
+// >>>>>>>>>>>>>
+// if using Apollo 2
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+server.applyMiddleware({ app });
+
+// <<<<<<<<<<<<<<>>>>>>>>>>>>>>
+
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static assets
@@ -37,5 +56,6 @@ app.get('*', (req, res) => {
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
